82dbe058e5942cde25f3de24ae047403b6078813
Dimuth De Zoysa <dimuthsakya@protonmail.com>
2021-02-17 16:10:15 +0530

    Update androidnetworkscan.py

--- androidnetworkscan.py
+++ androidnetworkscan.py
@@ -74,7 +74,7 @@ def anim() :                # Scanning animation
                 yield cursor
 
     spinner = spinning_cursor()
-    for _ in range(50):
+    for ii in range(50):
         sys.stdout.write(next(spinner))
         sys.stdout.flush()
         time.sleep(0.1)
